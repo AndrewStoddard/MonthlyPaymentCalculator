28146d4f962fb067deeb1ebffa5147c9daf165e4
AndrewStoddard <acs8182000@gmail.com>
2021-01-27 14:23:24 -0500

    Created simple model for monthly payment

---
+++ MonthlyPaymentCalculator/Models/MonthlyPayment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonthlyPaymentCalculator.Models
+{
+    public class MonthlyPayment
+    {
+        public int PurchasePrice { get; set; }
+        public double InterestRate { get; set; }
+        public int Years { get; set; }
+        public double MonthlyPaymentAmount => (InterestRate * PurchasePrice) / (1 - Math.Pow(1 + InterestRate, Years * 12));
+    }
+}
